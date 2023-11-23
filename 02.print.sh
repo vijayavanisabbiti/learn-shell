04b41a686c0704c4aaa91b5eb1b00e5ffaddb033
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-11-23 16:16:09 +0530

    expense project configuration

---
+++ 02.print.sh
@@ -0,0 +1,16 @@
+echo Hello World!
+
+# Color syntax: echo -e "\e[COLm MESSAGE \e[0m"
+# echo -e , -e is to allow colors
+# Double quotes are mandatory, Single quotes can be used but not preferred.
+# \e[COLm -> COL is a color code we need provide
+  # RED       - 31
+  # GREEN     - 32
+  # YELLOW    - 33
+  # BLUE      - 34
+  # MAGENTA   - 35
+  # CYAN      - 36
+# \e[0m -> THis is to disable the enabled color
+
+echo -e "\e[31m RED "
+echo -e "\e[32m GREEN \e[0m"
