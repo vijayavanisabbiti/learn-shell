9764f3e06b5dbabd41ed00c7c9cb0dc821b92e58
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-11-25 14:09:14 +0530

    expense project configuration

---
+++ 04.loops.sh
@@ -0,0 +1,11 @@
+# there are four loops
+# while and for loop
+# while for expression and for loop if you have inputs
+# Loop is used when you're trying to execute the same set of commands again and again in an iterative way
+
+a=12
+while ( '$a' -gt 0 ); do
+  echo 'Hello World'
+  a=$(($a-1))
+done
+
