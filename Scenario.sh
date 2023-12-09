dbcaa62f8974f5fa95ff64cf592d252993a29cb3
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-12-09 16:30:37 +0530

    test case

--- Scenario.sh
+++ Scenario.sh
@@ -1,9 +1,5 @@
 source joindevops.txt
-x=0
-want=11
-while read -r line; do
-   if [ $x -eq $want ]; then
-    echo ${line}
-    break
-  fi
+
+for LINE in $(cat "${source}"); do
+    echo "$LINE"
 done
